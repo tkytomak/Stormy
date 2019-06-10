d6415b5a014663a9351493f4cbdafdfd73483dfe
tkytomak <teemu.kytomaki@gmail.com>
2019-06-10 22:03:31 +0300

    Initial commit

---
+++ app/src/main/java/com/tkytomak/stormy/MainActivity.java
@@ -0,0 +1,53 @@
+package com.tkytomak.stormy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String apiKey = getString(R.string.apiKey);
+        double latitude = 37.8267;
+        double longtitude = -122.4233;
+        String forecastURL = "https://api.darksky.net/forecast/"
+                + apiKey + "/" + latitude + "," + longtitude;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(forecastURL)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "IO Exception caught: ", e);
+                }
+            }
+        });
+    }
+}
