00c254d7e8667a41102d2081dbee5e2e06ee0805
tkytomak <teemu.kytomaki@gmail.com>
2019-06-11 22:41:37 +0300

    Added CurrentWeather class

---
+++ app/src/main/java/com/tkytomak/stormy/CurrentWeather.java
@@ -0,0 +1,130 @@
+package com.tkytomak.stormy;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class CurrentWeather {
+
+    private String locationLabel;
+    private String icon;
+    private long time;
+    private double temperature;
+    private double humidity;
+    private double precipChance;
+    private String summary;
+    private String timeZone;
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
+
+    public String getLocationLabel() {
+        return locationLabel;
+    }
+
+    public void setLocationLabel(String locationLabel) {
+        this.locationLabel = locationLabel;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public int getIconId() {
+        int iconId = R.drawable.clear_day;
+
+        switch (icon) {
+
+            case "clear_day":
+                iconId = R.drawable.clear_day;
+                break;
+            case "clear_night":
+                iconId = R.drawable.clear_night;
+                break;
+            case "rain":
+                iconId = R.drawable.rain;
+                break;
+            case "snow":
+                iconId = R.drawable.snow;
+                break;
+            case "sleet":
+                iconId = R.drawable.sleet;
+                break;
+            case "wind":
+                iconId = R.drawable.wind;
+                break;
+            case "fog":
+                iconId = R.drawable.fog;
+                break;
+            case "cloudy":
+                iconId = R.drawable.cloudy;
+                break;
+            case "partly-cloudy-day":
+                iconId = R.drawable.partly_cloudy;
+                break;
+            case "partly-cloudy-night":
+                iconId = R.drawable.cloudy_night;
+                break;
+        }
+
+        return iconId;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public String getFormattedTime(long time) {
+        SimpleDateFormat formatter = new SimpleDateFormat("hh:mm:ss");
+
+        formatter.setTimeZone(TimeZone.getTimeZone(timeZone));
+
+        Date dateTime = new Date(time * 1000);
+        return formatter.format(dateTime);
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public double getPrecipChance() {
+        return precipChance;
+    }
+
+    public void setPrecipChance(double precipChance) {
+        this.precipChance = precipChance;
+    }
+}
